02e1ca220da03dea1009e05a8c3f4eac119223cf
ynwqmv <112755279+ynwqmv@users.noreply.github.com>
2023-01-08 01:02:39 -0800

    small changes

---
+++ Xeditor upd/main.cpp
@@ -0,0 +1,52 @@
+#include "mainwindow.h"
+
+#include <QApplication>
+#include <QtWidgets>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QPushButton>
+#include <QStatusBar>
+#include <QString>
+#include <QStringList>
+#include <QStyleFactory>
+#include <QIcon>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+
+
+
+
+    /*QLabel *label = new QLabel("Hello, World!");
+       QPushButton *button = new QPushButton("Click me");
+       QPushButton *button2 = new QPushButton("Count");
+
+       // Create the layout and add the widgets to it
+       QVBoxLayout *layout = new QVBoxLayout;
+
+       layout->addWidget(label);
+       layout->addWidget(button);
+       layout->addWidget(button2);
+
+       // Set the layout as the central widget of the window
+       w.setCentralWidget(new QWidget);
+       w.centralWidget()->setLayout(layout);
+
+
+       int clickCount = 0;
+       QObject::connect(button, &QPushButton::clicked, [label]()
+       { label->setText("Button was clicked!");});
+
+
+
+       QObject::connect(button2, &QPushButton::clicked, [&]() {
+              if (++clickCount >= 10) {
+                  button2->setEnabled(false);
+
+              w.statusBar()->showMessage(QString("Button was clicked %1 times").arg(clickCount));
+       }});*/
+    w.show();
+    return a.exec();
+}
